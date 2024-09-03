98dbbc688e738986e3f6552721d7b907253475a9
Vinit Motwani <vinitmotwani10@gmail.com>
2024-09-03 16:32:31 -0700

    added to main

---
+++ classification.py
@@ -0,0 +1,18 @@
+JOB_APPLICATION_CONFIRMATION = "Job Applied Confirmation"
+JOB_REJECTION = "Job Rejection"
+JOB_UNDER_CONSIDERATION = "Job Under Consideration"
+JOB_OFFERED = "Job Offered"
+JOB_COMPLETED_TASK = "Completed task corresponding to a Job"
+NEW_JOB_NOTIFICATION = "New Job Notification"
+NEW_JOB_TASK = "Job Task or To Do"
+NOT_JOB_SPECIFIC_EMAIL = "Not Job Specific Email"
+
+Classifications = [
+    JOB_APPLICATION_CONFIRMATION,
+    JOB_REJECTION,
+    JOB_OFFERED,
+    JOB_COMPLETED_TASK,
+    NEW_JOB_NOTIFICATION,
+    NEW_JOB_TASK,
+    NOT_JOB_SPECIFIC_EMAIL
+]
